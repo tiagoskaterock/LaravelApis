ae146ca38d2c07a5dc61ad88dc31dc82bf42ca89
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-08-31 02:57:30 -0300

    Resource Routes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
 use Illuminate\Http\Request;
 use App\Models\Task;
 
 class TaskController extends Controller
 {
+
     function index() {
         return new TaskCollection(Task::all());
         // return response()->json(Task::all());
     }
+
+
+    function show(Request $request, Task $task) {
+        return new TaskResource($task);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('tasks', [TaskController::class, 'index']);
+// Route::get('tasks', [TaskController::class, 'index']);
+
+Route::apiResource('tasks', TaskController::class)->only([
+    'index', 'show'
+]);
