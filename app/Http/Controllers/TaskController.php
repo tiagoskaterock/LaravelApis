701f532f9ac6a68bb1a091d59b31228347f83865
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-09-04 00:28:27 -0300

    7 - Updating a resource

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,26 +5,29 @@ namespace App\Http\Controllers;
 use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use Illuminate\Http\Request;;
 use App\Models\Task;
 
-class TaskController extends Controller
-{
+class TaskController extends Controller {
 
     function index() {
         return new TaskCollection(Task::all());
-        // return response()->json(Task::all());
     }
-
 
     function show(Request $request, Task $task) {
         return new TaskResource($task);
     }
 
-
     function store(StoreTaskRequest $request) {        
         $validated = $request->validated();
         $task = Task::create($validated);
+        return new TaskResource($task);
+    }
+
+    function update(UpdateTaskRequest $request, Task $task) {
+        $validated = $request->validated();
+        $task->update($validated);
         return new TaskResource($task);
     }
 
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -18,7 +18,7 @@ class Task extends Model {
     ];
 
     protected $fillable = [
-        'title'
+        'title', 'is_done'
     ];
 
 }
