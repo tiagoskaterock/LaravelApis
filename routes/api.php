0e4f37aae2e953da92f996c91d9fa78bd31cf53f
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-14 01:23:16 -0300

    13. Logging in With Our User

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
